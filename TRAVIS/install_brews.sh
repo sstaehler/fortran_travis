009ea7bcd93ccdadbc582c621646e2a6e39b3fc5
Simon Stähler <staehler@geophysik.uni-muenchen.de>
2017-01-20 12:49:13 +0100

    download brew cache from Google Drive

--- TRAVIS/install_brews.sh
+++ TRAVIS/install_brews.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
-if [ -f "$TRAVIS_ROOT/brew_cache.tar.gz" ]; then
-  echo "Found Brew Cache"
-  tar zxf $TRAVIS_ROOT/brew_cache.tar.gz --directory /usr/local/Cellar
-  brew link gmp libmpc isc gcc szip hdf5 openmpi fftw
+./gdrive download 0BwU9d5SH6pPQUFY0TU50RU5jRkk
+brew update
 
-else
-  echo "Found Brew Cache"
-  brew install openmpi 
-  brew install fftw --with-fortran;
-  brew tap homebrew/science;
-  brew install netcdf --with-fortran;
-  tar czf $TRAVIS_ROOT/brew_cache.tar.gz --directory /usr/local/Cellar gmp libmpc isc gcc szip hdf5 openmpi fftw
-  ./gdrive upload $TRAVIS_ROOT/brew_cache.tar.gz 
-fi
+#if [ -f "brew_cache.tar.gz" ]; then
+#  echo "Found Brew Cache, linking"
+#  tar zxf brew_cache.tar.gz --directory /usr/local/Cellar
+#  brew link gmp libmpc gcc szip hdf5 openmpi fftw #isc
+#
+# else
+  echo "Did not find Brew Cache, download and reinstall"
+brew install openmpi 
+brew install fftw --with-fortran;
+brew tap homebrew/science;
+brew install netcdf --with-fortran
+echo "*****************************************************************"
+brew info --installed
+echo "*****************************************************************"
+ls -l /usr/local/Cellar
+echo "*****************************************************************"
+tar czf brew_cache.tar.gz --directory /usr/local/Cellar gmp libmpc isc gcc szip hdf5 openmpi fftw netcdf
+./gdrive upload brew_cache.tar.gz 
+#fi
